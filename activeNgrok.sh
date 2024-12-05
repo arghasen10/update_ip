7667caee4db341e770b0a3e322645fa1cbbd5289
arghasen10 <arghasen10@gmail.com>
2024-12-05 19:27:41 +0530

    update ip

--- activeNgrok.sh
+++ activeNgrok.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 screen -S ngrok_screen -l -Logfile /home/argha/output.log -dm /snap/bin/ngrok tcp 22
+sleep 2
 /usr/bin/python3 /home/argha/update_ip/pyscript_to_ip_ngrok.py
 cd /home/argha/update_ip && git add . && git commit -m "update ip"
 cd /home/argha/update_ip && git push origin main
