3258bd062b74e14385aad79a6b703accb4f92970
arghasen10 <arghasen10@gmail.com>
2024-12-05 19:41:28 +0530

    update ip

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
- - IP: 0.tcp.ngrok.io
- - Port: 11304
+ - IP: 0.tcp.in.ngrok.io
+ - Port: 18709
--- activeNgrok.sh
+++ activeNgrok.sh
@@ -4,4 +4,15 @@ screen -S ngrok_screen -l -Logfile /home/argha/output.log -dm /snap/bin/ngrok tc
 /usr/bin/python3 /home/argha/update_ip/pyscript_to_ip_ngrok.py
 cd /home/argha/update_ip && git add . && git commit -m "update ip"
 cd /home/argha/update_ip && git push origin main
-wait
+
+# Name of the screen session to monitor
+SCREEN_NAME="ngrok_screen"
+
+echo "Waiting for screen session '$SCREEN_NAME' to close..."
+
+# Loop until the screen session is no longer running
+while screen -list | grep -q "$SCREEN_NAME"; do
+  sleep 1  # Wait for 1 second before checking again
+done
+
+echo "Screen session '$SCREEN_NAME' is closed."
